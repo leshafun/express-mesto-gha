90d5bd6c6b63d8bdd5261ea6d5cab16f915f80cc
leshafun <leshafun19@yandex.ru>
2022-11-26 23:25:07 +0700

    PR13.2

--- controllers/users.js
+++ controllers/users.js
@@ -16,7 +16,7 @@ module.exports.getUsers = (req, res) => {
 
 //возвращает пользователя по _id
 module.exports.getUser = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((user) => {
       if (user === null) {
         res.status(NOT_FOUND)

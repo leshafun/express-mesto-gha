379ff8171ade39a8b75a635d5b980b41de20442b
leshafun <leshafun19@yandex.ru>
2022-12-04 00:22:23 +0700

    PR14

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const NotFound = require('./errors/NotFound');
 const errorHandler = require('./middlewares/errorHandler');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 
@@ -14,12 +15,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/users', routerUsers);
-app.use('/cards', routerCards);
+app.use('/users', auth, routerUsers);
+app.use('/cards', auth, routerCards);
 app.post('/signin', routerUsers);
 app.post('/signup', routerUsers);
-app.use('/', (req, res) => {
-  res.status(NotFound).send({ message: 'Введенный адрес не найден' });
+app.use('/', auth, (req, res, next) => {
+  next(new NotFound('Введенный адрес не найден'));
 });
 
 app.listen(PORT);

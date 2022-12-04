255a7108cfb42c94fcf55b6b312b213ca0116300
leshafun <leshafun19@yandex.ru>
2022-12-04 22:43:24 +0700

    PR14.1

--- routes/cards.js
+++ routes/cards.js
@@ -19,13 +19,13 @@ cardRouter.post('/', celebrate({
 
 cardRouter.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().hex().length(24),
+    cardId: Joi.string().hex().length(),
   }),
 }), deleteCard);
 
 cardRouter.put('/:cardId/likes', celebrate({
   params: Joi.object().keys({
-    cardId: Joi.string().length(24).hex(),
+    cardId: Joi.string().length().hex(),
   }),
 }), likeCard);
 
